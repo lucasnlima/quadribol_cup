f989e33a717955035b97107fcc7391bdc58fa617
Lucas Lima <lucas.lima@ufmg.br>
2021-03-07 15:11:53 -0300

    Innital commit with code

---
+++ quadribol-api/src/main/java/quadribolapi/controller/UserController.java
@@ -0,0 +1,32 @@
+package quadribolapi.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import quadribolapi.domain.User;
+import quadribolapi.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepoitory;
+	
+	@GetMapping
+	public List<User> getUsers() {
+		return userRepoitory.findAll();
+	}
+	
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userRepoitory.save(user);
+	}
+}
