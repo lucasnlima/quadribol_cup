fef41ab5bb27b13b1584c6c330e77ca1e8ed1447
joao.ferreira <joao.ferreira@concert.com.br>
2021-03-11 00:54:01 -0300

    classes_joao

--- quadribol-api/src/main/java/quadribolapi/domain/Jogo.java
+++ quadribol-api/src/main/java/quadribolapi/domain/Jogo.java
@@ -7,30 +7,106 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class Jogo {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private final Long id;
 	
 	@Column(nullable = false)
-	private String user;
+	private String data;
 
-	public Long getId() {
+    @Column(nullable = false)
+	private Placar placar;
+
+    @Column(nullable = false)
+	private Equipe participanteA;
+
+    @Column(nullable = false)
+	private Equipe participanteB;
+
+    @Column(nullable = false)
+	private Arbitro arbitro;
+
+    @Column(nullable = false)
+	private PracaEsportiva local;
+
+	public Jogo(Long ide, String dat, Placar plac, Equipe partA, Equipe partB, Arbitro arb, PracaEsportiva loc) {
+        this.id = ide;
+        this.data = dat;
+        this.placar = plac;
+        this.participanteA = partA;
+        this.participanteB = partB;
+        this.arbitro = arb;
+        this.local = loc;
+    }
+    
+    public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public String getData() {
+		return this.data;
 	}
 
-	public String getUser() {
-		return user;
+	public void setData(String novad) {
+		this.data = novad;
 	}
 
-	public void setUser(String user) {
-		this.user = user;
+    public Placar getPlacar() {
+		return this.placar;
 	}
-	
+
+	public void setPlacar(Placar novap) {
+		this.placar = novap;
+	}
+
+    public Equipe getParticipanteA() {
+		return this.participanteA;
+	}
+
+    public void setParticipanteA(Equipe novaeqa) {
+		this.participanteA = novaeqa;
+	}
+
+    public Equipe getParticipanteB() {
+		return this.participanteB;
+	}
+
+    public void setParticipanteB(Equipe novaeqb) {
+		this.participanteB = novaeqb;
+	}
+
+    public Arbitro getArbitro() {
+		return this.arbitro;
+	}
+
+	public void setArbitro(Arbitro novoa) {
+		this.arbitro = novoa;
+	}
+
+    public PracaEsportiva getPracaEsportiva() {
+		return this.local;
+	}
+
+	public void setPracaEsportiva(PracaEsportiva novol) {
+		this.local = novol;
+	}
+
+    public void exibirInfoJogo() {
+        
+        System.out.printf("ID: 0x%016X", this.getId());
+        System.out.printf("Participantes: %s x %s", this.getParticipanteA(), this.getParticipanteB()); // colocar nome da equipe
+        System.out.printf("Arbitro: %s", this.getArbitro().getNome());
+        System.out.printf("Local: %s", this.getPracaEsportiva().getNome());
+        System.out.printf("Placar: %s", this.getPlacar().getEstatisticas());
+
+    }
+
+    public void alterarJogo() {
+        
+        // FAZER
+        
+    }
 
 }
