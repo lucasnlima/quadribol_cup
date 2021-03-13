9b6f674581afc8f525655811116369ddbcdcdbd8
Luiz <lgsf@outlook.com>
2021-03-13 14:10:11 -0300

    Adicionando classes fase e TabelaJogos

---
+++ quadribol-api/src/main/java/quadribolapi/domain/Fase.java
@@ -0,0 +1,60 @@
+package quadribolapi.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fase {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private final Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+
+    @Column(nullable = false)
+	private int numeroParticipantes;
+
+
+	public Fase(Long id, String nome, int participantes) {
+        this.id = id;
+        this.nome = nome;
+        this.numeroParticipantes = participantes;
+    }
+    
+    public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+
+    public int getNumeroParticipantes() {
+		return this.numeroParticipantes;
+	}
+
+	public void setNumeroParticipantes(int novoNumero) {
+		this.numeroParticipantes = novoNumero;
+	}
+
+    
+    public void incluirFase(Long id, String nome, int participantes) {
+        Fase novaFase = new Fase(id, nome, participantes);
+		return novaFase;
+    }
+
+    public void alterarFase(String nome, int participantes) {
+        this.setNome(nome);
+		this.setNumeroParticipantes(participantes);
+    }
+
+}
---
+++ quadribol-api/src/main/java/quadribolapi/domain/TabelaJogos.java
@@ -0,0 +1,55 @@
+package quadribolapi.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TabelaJogos {
+	
+	@Column(nullable = false)
+	private Fase fase;
+	
+	@Column(nullable = false)
+	private Jogo partidas;
+
+    @Column(nullable = false)
+	private String chaveamento;
+
+
+	public TabelaJogos(Fase fase, Jogo jogo, String chave) {
+        this.fase = fase;
+        this.partidas = jogo;
+        this.chaveamento = chave;
+    }
+    
+
+    public String getChaveamento() {
+		return this.chaveamento;
+	}
+
+	public void setChaveamento(String novoChaveamento) {
+		this.chaveamento = novoChaveamento;
+	}
+
+    
+    public void consultarTabela(Long id, String nome, int participantes) {
+        //fazer
+    }
+
+    public void inicializarTabela(String nome, int participantes) {
+        //fazer
+    }
+	
+	public void alterarTabela(String nome, int participantes) {
+        //fazer
+    }
+
+	public void definirChaveamento(String nome, int participantes) {
+        //fazer
+    }
+
+
+}
